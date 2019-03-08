bb2e7daeba7ec7c44e91448f3d6e869eb00e3c3b
Matt Basile <matthewbasile2@gmail.com>
2019-03-08 12:08:23 -0500

    Tests for Add()/POST done

--- index.spec.js
+++ index.spec.js
@@ -5,9 +5,22 @@ describe('API TESTING', () => {
     afterAll(async ()=>{
         await db('games').truncate();
        });
-    it('post method', async() => {
-        const game = await Games.add({title: 'Battleship', genre: "Arcade", releaseYear: 1969})
-            
-        expect(game.title).toBe('Battleship')
+    describe('ADD() ', () => {
+        it('Adding one Game full Content', async() => {
+            const game = await Games.add({title: 'Battleship', genre: "Arcade", releaseYear: 1969})  
+            expect(game.title).toBe('Battleship')
         });
+        it('Adding a Game with No Year', async() => {
+            const game = await Games.add({title: 'Wanda', genre: "Arcade",})
+            expect(game.title).toBe('Wanda')
+        });
+        it('Adding a Multiple Games', async() => {
+            await Games.add({title: 'Tetris', genre: "Arcade", releaseYear: 1979})
+            await Games.add({title: 'Donkey Kong', genre: "Arcade", releaseYear: 1977})
+            const games = await db('games')
+            expect(games).toHaveLength(4)
+        });
+        
+    });
+   
     });
